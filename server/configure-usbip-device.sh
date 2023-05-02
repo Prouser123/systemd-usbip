a452161254e8b870b04fba1344552acd5b036056
James Cahill <jcahill773@gmail.com>
2023-05-02 10:48:22 +0100

    Experiment with state changed auto reattach (not sure if even needed, might be impl'd in existing code)

--- server/configure-usbip-device.sh
+++ server/configure-usbip-device.sh
@@ -1,4 +1,24 @@
 #!/bin/bash -e
-BUS_ID=`/usr/sbin/usbip list -p -l | grep -i "#usbid=${USB_IDVENDOR}:${USB_IDPRODUCT}#" | cut '-d#' -f1`
-[[ -z "$BUS_ID" ]] || /usr/sbin/usbip bind --$BUS_ID
 
+previousState = 1
+state = $(/usr/bin/lsusb | grep -q ${USB_IDVENDOR}:${USB_IDPRODUCT}; echo $?)
+
+
+TMPFILE=/tmp/usbip-${USB_IDVENDOR}-${USB_IDPRODUCT}
+
+touch $TMPFILE
+
+# Check to see if the tmp file exists (will be deleted when service is stopped)
+while [[ -e $TMPFILE ]]
+do
+    # Check to see if the state has changed
+    if [ $state -ne $previousState]; then
+        BUS_ID=`/usr/sbin/usbip list -p -l | grep -i "#usbid=${USB_IDVENDOR}:${USB_IDPRODUCT}#" | cut '-d#' -f1`
+        [[ -z "$BUS_ID" ]] || /usr/sbin/usbip bind --$BUS_ID
+    fi
+    
+    # Update the state
+    sleep 1
+    previousState=$state
+    state = $(/usr/bin/lsusb | grep -q ${USB_IDVENDOR}:${USB_IDPRODUCT}; echo $?)
+done
--- server/remove-usbip-device.sh
+++ server/remove-usbip-device.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -e
+
+# Remove the temporary marker (signals the configure script to stop running)
+rm /tmp/usbip-${USB_IDVENDOR}-${USB_IDPRODUCT}
+
+# Find and unbind the device
 BUS_ID=`/usr/sbin/usbip list -p -l | grep -i "#usbid=${USB_IDVENDOR}:${USB_IDPRODUCT}#" | cut '-d#' -f1`
 [[ -z "$BUS_ID" ]] || /usr/sbin/usbip unbind --$BUS_ID
 
